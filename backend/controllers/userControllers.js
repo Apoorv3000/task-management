36fbbbf328fb89642c2ae031c0a74417b48d570e
Apoorv Kumar <apoorvkumarvbcv@gmail.com>
2023-02-06 22:11:09 +0530

    implemented user crud operations

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,1 +1,30 @@
 import User from "../models/UserModel.js";
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({
+      _id: { $ne: req.user.id },
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted.");
+  } catch (err) {
+    next(err);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ dotenv.config();
 const app = express();
 
 //db connection
+
 const connect = () => {
   mongoose
     .connect(process.env.MONGO)
@@ -21,17 +22,33 @@ const connect = () => {
 };
 
 //remove the mongoose warning
+
 mongoose.set("strictQuery", true);
 
 // port
 const port = process.env.PORT || 5001;
 
 // middlewares
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
-// connects to the server
+//error handling
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+// listens to the server
+
 app.listen(port, () => {
   connect();
   console.log("Connected to server at port: " + port);
